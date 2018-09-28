42d5a4f02f7a6802be989374083fb4ecac4cb8f7
kirillGovor <bns223@mail.ru>
2018-09-28 11:43:31 +0300

    c

--- filter/filter.js
+++ filter/filter.js
@@ -4,34 +4,43 @@ var filter = React.createClass({
 
 
   propTypes: {
-    catalog: React.PropTypes.arrayOf(
+    
+    words: React.PropTypes.arrayOf(
       React.PropTypes.shape({
         code: React.PropTypes.number.isRequired,
         text: React.PropTypes.string.isRequired,
+        
       }),
     ),
 
+  },
+
+  sort:function(){
+this.setState({text:this.state.code})
+console.log(text);
   },
 
 
 
 
 
-
-
   render: function () {
-    var catalogCode = this.props.catalog.map(v =>
+    var catalogCode = this.props.words.map(v =>
       React.DOM.p({ className: 'text', key: v.code, }, v.text),
     );
+
+    catalogCode2={catalogCode,sorted:this.props.sorted='false',contain:this.props.contain = '',}
+  //  this.state.words={catalogCode}
    // console.log(catalogCode[0].props.children);
     return React.DOM.div(null,
       React.DOM.input({
-        type: 'checkbox', name: 'check', className: 'Check',defaultChecked:'false'
+        type: 'checkbox', name: 'check', className: 'Check',onClick:this.sort,//defaultChecked:'false'
       }),
       React.DOM.input({
-        type: 'text', name: 'text', className: 'text',
+        type: 'text', name: 'text', className: 'text', 
       }),
       React.DOM.div({ className: 'divText', }, catalogCode),
+      console.log(catalogCode2),
       
     );
 
