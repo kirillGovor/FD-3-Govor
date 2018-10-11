e7e77bac4a56de47a2e951a567eef3d6d8ab0d18
kirillGovor <bns223@mail.ru>
2018-10-11 11:30:31 +0300

    Commit

--- ishop2/ishop.js
+++ ishop2/ishop.js
@@ -2,10 +2,23 @@ var ishop = React.createClass({
 
   displayName: 'shop',
 
+  propTypes: {
+    ishop:React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.string.isRequired,
+        kolvo: React.PropTypes.number.isRequired,
+        cost: React.PropTypes.number.isRequired,
+        url: React.PropTypes.string.isRequired,
+       
+      })
+    )
+  },
+
   getInitialState: function () {
     return {
       ishop: this.props.catalog,
       delited: "",
+
     }
 
   },
@@ -13,16 +26,22 @@ var ishop = React.createClass({
 
 
   delete: function (EO) {
-    event.stopPropagation();
+   
+    EO.stopPropagation();
+    EO.nativeEvent.stopImmediatePropagation();
     let contain = this.state.ishop.slice();
     contain=contain.filter(function(number) {
       return (number.code !=EO.target.id) ;
     });
-    this.setState({ ishop: contain });
+    this.setState({ ishop: contain, },this.color());
   },
 
   color:function(EO){
-    event.stopPropagation();
+    if (EO==undefined){
+      let contain = this.state.ishop.slice();
+      this.setState({ ishop: contain });
+    }
+    else{
     let contain = this.state.ishop.slice();
     for (i = 0; i <= contain.length - 1; i++) {
       if (contain[i].code == EO.target.id) {
@@ -35,6 +54,7 @@ var ishop = React.createClass({
       }
     }
     this.setState({ ishop: contain });
+  }
   },
 //id:"color"+v,
 //onClick: this.color,
@@ -42,37 +62,14 @@ var ishop = React.createClass({
 
 
   render: function () {
-    var catalogCode = this.state.ishop.map(v =>
-      React.DOM.tbody({ id:v.code, onClick: this.color, key: v.code, className: 'block',  },
-
-        React.DOM.tr({id:"color"+v.code, onClick: this.color, className: 'tr', },
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count', }, v.code,
-
-          ),
-          React.DOM.td({id:v.code, onClick: this.color,}, React.DOM.img({  id:v.code, onClick: this.color, className: 'Count', src: v.url, width: 100, height: 100, }), ),
-
-          React.DOM.td({  id:v.code, onClick: this.color,className: 'Count' }, v.cost),
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count' }, v.kolvo, ),
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count' },
-            React.DOM.input({ id: v.code, className: 'input', value: 'delete', type: 'submit', onClick:this.delete, }),
-          ),
-        ),
-      )
-    );
-    
+  
 
     
     return React.DOM.div({ className: 'VotesBlock' },
-        React.DOM.div({ className: 'Question' }, this.props.MainText),
+        React.createElement(ishopFunctionality,{MainText:text,catalog:this.props.catalog, color:this.color,delete:this.delete,ishop: this.state.ishop,}),
+        React.DOM.div({ className: 'Question' },),
         React.DOM.table({ className: 'table' }, 
-        React.DOM.tbody({ className: 'block' },
-        React.DOM.tr({ className: 'tr', },
-        React.DOM.td({ className: 'Count', key:"MainTr"+1 }, 'Название: '),
-        React.DOM.td({ className: 'Count', key:"MainTr"+2}, 'Изображение: '),
-        React.DOM.td({ className: 'Count',key:"MainTr"+3 }, 'Цена: '),
-        React.DOM.td({ className: 'Count',key:"MainTr"+4 }, 'Количество: '),
-        React.DOM.td({ className: 'Count',key:"MainTr"+5 }, 'Свойства: '),
-      ),), catalogCode),
+    ),
       
     );
   },
--- ishop2/ishopFunctionality.js
+++ ishop2/ishopFunctionality.js
@@ -2,34 +2,64 @@ var ishopFunctionality = React.createClass({
 
     displayName: 'ishopFunctionality',
 
+    propTypes: {
+      delete: React.PropTypes.func.isRequired,
+      color: React.PropTypes.func.isRequired,
+      ishop:React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          code: React.PropTypes.string.isRequired,
+          kolvo: React.PropTypes.number.isRequired,
+          cost: React.PropTypes.number.isRequired,
+          url: React.PropTypes.string.isRequired,
+         
+        })
+      )
+    },
 
- 
+    getInitialState: function () {
+      return {
+        
+      }
+  
+    },
   
   
 
     
   render: function () {
-    var catalogCode = this.state.ishop.map(v =>
+    var catalogCode = this.props.ishop.map(v =>
       React.DOM.tbody({ id:v.code, onClick: this.color, key: v.code, className: 'block',  },
 
-        React.DOM.tr({id:"color"+v.code, onClick: this.color, className: 'tr', },
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count', }, v.code,
-
+        React.DOM.tr({id:"color"+v.code, onClick: this.props.color, className: 'tr', },
+          React.DOM.td({ id:v.code, className: 'Count', }, v.code,
           ),
-          React.DOM.td({id:v.code, onClick: this.color,}, React.DOM.img({  id:v.code, onClick: this.color, className: 'Count', src: v.url, width: 100, height: 100, }), ),
-
-          React.DOM.td({  id:v.code, onClick: this.color,className: 'Count' }, v.cost),
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count' }, v.kolvo, ),
-          React.DOM.td({ id:v.code, onClick: this.color, className: 'Count' },
-            React.DOM.input({ id: v.code, className: 'input', value: 'delete', type: 'submit', onClick:this.delete, }),
+          React.DOM.td({id:v.code,}, React.DOM.img({  id:v.code,className: 'Count', src: v.url, width: 100, height: 100, }), ),
+          React.DOM.td({  id:v.code,className: 'Count' }, v.cost),
+          React.DOM.td({ id:v.code, className: 'Count' }, v.kolvo, ),
+          React.DOM.td({ id:v.code, className: 'Count' },
+            React.DOM.input({ id: v.code, className: 'input', value: 'delete', type: 'submit', onClick:this.props.delete }),
           ),
         ),
       )
     );
     
-    },
 
-
-
+    
+    return React.DOM.div({ className: 'VotesBlock' },
+        React.DOM.div({ className: 'Question' }, this.props.MainText),
+        React.DOM.table({ className: 'table' }, 
+        React.DOM.tbody({ className: 'block' },
+        React.DOM.tr({ className: 'tr', },
+        React.DOM.td({ className: 'Count', key:"MainTr"+1 }, 'Название: '),
+        React.DOM.td({ className: 'Count', key:"MainTr"+2}, 'Изображение: '),
+        React.DOM.td({ className: 'Count',key:"MainTr"+3 }, 'Цена: '),
+        React.DOM.td({ className: 'Count',key:"MainTr"+4 }, 'Количество: '),
+        React.DOM.td({ className: 'Count',key:"MainTr"+5 }, 'Свойства: '),
+      ),), catalogCode),
+      
+    );
+  },
 
 });
+
+
