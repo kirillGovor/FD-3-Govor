ae9889b91ed95a92485ca2e2749785faefbca76b
kirillGovor <bns223@mail.ru>
2018-10-26 17:31:44 +0300

    c

---
+++ VotesBlock480_children/components/RainbowFrame.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+   
+  };
+  
+  render() {
+    var atr=this.props.children;
+    var colorg = this.props.color.forEach(element => {
+      atr=<div style={{border:"dashed 1px "+String(element.color),padding:"1px"}}>{atr}</div>
+    });
+    return (
+      /*
+      <div style={{border:"dashed 1px "+this.props.color[0].color,padding:"10px"}}>
+        {this.props.children}
+      </div>
+      */
+     atr
+    );
+  }
+
+}
+
+export default RainbowFrame;
